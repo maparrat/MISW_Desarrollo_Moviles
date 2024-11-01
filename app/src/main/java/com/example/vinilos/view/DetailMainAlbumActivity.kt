f03b0d61ab88a73cce44d39e4aa484d52b7f9c32
devlucasblandon <l.blandon@uniandes.edu.co>
2024-10-31 19:42:58 -0500

    se agrega bundle funcional

--- app/src/main/java/com/example/vinilos/view/DetailMainAlbumActivity.kt
+++ app/src/main/java/com/example/vinilos/view/DetailMainAlbumActivity.kt
@@ -7,20 +7,15 @@
 package com.example.vinilos.view
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import com.example.vinilos.R
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.vinilos.R.*
-import com.example.vinilos.R.id.main
+import com.example.vinilos.R.layout
 
 
 class DetailMainAlbumActivity : AppCompatActivity() {
@@ -30,6 +25,20 @@ class DetailMainAlbumActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(layout.activity_detail_album)
+
+        val bundle = intent.extras
+        //Log.d(bundle.toString(), "bundle ver")
+
+        val b = bundle?.getInt("albumId")
+        var a = bundle?.getString("albumId")
+         a = "tonces "
+
+        val textView = findViewById<TextView>(R.id.albumTitle)
+        textView.text = a
+        //Toast.makeText(this, a, Toast.LENGTH_SHORT).show()
+
+        //val  a  = intent.getStringExtra("albumId")
+        //Toast.makeText(this, a, Toast.LENGTH_SHORT).show()
 
 
     }
--- app/src/main/java/com/example/vinilos/view/adapters/albumAdapter.kt
+++ app/src/main/java/com/example/vinilos/view/adapters/albumAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.vinilos.view.adapters
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.LayoutRes
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
@@ -11,6 +14,7 @@ import com.example.vinilos.R
 import com.example.vinilos.model.AlbumModel
 import com.example.vinilos.view.AlbumFragment
 import com.example.vinilos.databinding.ActivityListAlbumBinding
+import com.example.vinilos.view.DetailMainAlbumActivity
 
 
 class AlbumsAdapter: RecyclerView.Adapter<AlbumsAdapter.AlbumViewHolder>() {
@@ -49,7 +53,24 @@ class AlbumsAdapter: RecyclerView.Adapter<AlbumsAdapter.AlbumViewHolder>() {
         holder.viewDataBinding.root.setOnClickListener {
             val album = holder.viewDataBinding.album // Acceder a los datos asociados a la vista
 
-            Toast.makeText(holder.context, "Has seleccionado: ${albums[position]} ", Toast.LENGTH_SHORT).show()
+            val intent = Intent(holder.context, DetailMainAlbumActivity::class.java)
+            intent.putExtra("albumId", albums[position].id)
+            ContextCompat.startActivity(holder.context, intent, null)
+
+
+
+
+          //  val intent = Intent(holder.context, DetailMainAlbumActivity::class.java)
+
+          //  intent.putExtra("albumId", albums[position].id)
+
+          //  ContextCompat.startActivity(holder.context, intent, null)
+
+            //intent.putExtra("albumId", albums[position].id)
+
+           // Toast.makeText(holder.context, "Has seleccionado: ${album.name} ", Toast.LENGTH_SHORT).show()
+
+          //  Toast.makeText(holder.context, "Has seleccionado: ${albums[position]} ", Toast.LENGTH_SHORT).show()
         }
 //        holder.viewDataBinding.root.setOnClickListener {
 //            val action = AlbumFragmentDirections.actionAlbumFragmentToCommentFragment(albums[position].albumId)
@@ -57,6 +78,7 @@ class AlbumsAdapter: RecyclerView.Adapter<AlbumsAdapter.AlbumViewHolder>() {
 //            holder.viewDataBinding.root.findNavController().navigate(action)
 //        }
     }
+
 
     override fun getItemCount(): Int {
         return albums.size

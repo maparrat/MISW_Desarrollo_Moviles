ca9749009e44654da195fffe13c1751c0fc1f691
Carlos Rios <c.riosp@uniandes.edu.co>
2024-11-10 13:58:40 -0500

    Implementación de Corutinas a H1

--- app/src/main/java/com/example/vinilos/repositories/albumRepository.kt
+++ app/src/main/java/com/example/vinilos/repositories/albumRepository.kt
@@ -1,52 +1,41 @@
 package com.example.vinilos.repositories
 import android.app.Application
+import androidx.compose.ui.window.application
 import com.android.volley.VolleyError
 import com.example.vinilos.model.AlbumModel
 import com.example.vinilos.model.AlbumDetailModel
 import com.example.vinilos.network.NetworkServiceAdapter
 
-class albumRepository (val application: Application){
-
-    fun refreshData(callback: (List<AlbumModel>)->Unit, onError: (VolleyError)->Unit) {
+class albumRepository (val application: Application) {
+    suspend  fun refreshData(): List<AlbumModel> {
         //Determinar la fuente de datos que se va a utilizar. Si es necesario consultar la red, ejecutar el siguiente código
-        NetworkServiceAdapter.getInstance(application).getAlbums({
-            //Guardar los albumes de la variable it en un almacén de datos local para uso futuro
-            callback(it)
-        },
-            onError
-        )
-
-//        private fun refreshDataFromNetwork() {
-//            NetworkServiceAdapter.getInstance(getApplication()).getAlbums({
-//                _albums.postValue(it)
-//                _eventNetworkError.value = false
-//                _isNetworkErrorShown.value = false
-//            },{
-//                //Log.d("Error", it.toString())
-//                _eventNetworkError.value = true
-//            })
-//        }
-
-    }
-
-    fun refreshDetailData(albumId: Int, callback: (List<AlbumDetailModel>)->Unit, onError: (VolleyError)->Unit) {
-        //Determinar la fuente de datos que se va a utilizar. Si es necesario consultar la red, ejecutar el siguiente código
-
-
-        NetworkServiceAdapter.getInstance(application).getAlbumDetails(albumId, {
-            //Guardar los albumes de la variable it en un almacén de datos local para uso futuro
-            albumDetail ->
-            callback(albumDetail)
-        },
-            onError
-        )
-
-//
-
+        return NetworkServiceAdapter.getInstance(application).getAlbums()
     }
 
 
+    fun refreshDetailData(
+        albumId: Int,
+        callback: (List<AlbumDetailModel>) -> Unit,
+        onError: (VolleyError) -> Unit
+    ) {
+        //Determinar la fuente de datos que se va a utilizar. Si es necesario consultar la red, ejecutar el siguiente código
 
 
+        NetworkServiceAdapter.getInstance(application).getAlbumDetails(
+            albumId, {
+                //Guardar los albumes de la variable it en un almacén de datos local para uso futuro
+                    albumDetail ->
+                callback(albumDetail)
+            },
+            onError
+        )
+
+
+    }
 }
+
+
+
+
+
 
